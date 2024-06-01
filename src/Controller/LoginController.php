0ea0ebe1e0880b6baf45047e026ca8cc5360922c
yasmine <yasmine.elhakem@insat.ucar.tn>
2024-06-01 15:15:27 +0100

    see applications

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -18,10 +18,12 @@ class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
+
         return $this->render('login/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+
     }
 
     #[Route(path: '/logout', name: 'app_logout')]
